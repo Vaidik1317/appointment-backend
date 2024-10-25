da84cc6a7ea72ba7164b3eb9c60b586edae9b6a6
Vaidik <127821329+Vaidik1317@users.noreply.github.com>
2024-10-25 18:05:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Change this to the specific origin of your frontend
+    credentials: true, // Allow cookies if needed
+  })
+);
 app.use(bodyParser.json());
 
 mongoose.connect(
